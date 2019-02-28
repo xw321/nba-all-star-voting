918955125a7a9254513e99fcc01bb3ceb5c4a04c
xunwang <wang.xun2@husky.neu.edu>
2019-02-27 17:39:29 -0800

    config express listen port

--- app.js
+++ app.js
@@ -36,4 +36,12 @@ app.use(function(err, req, res) {
   res.render("error");
 });
 
+app.listen(process.env.PORT || 3000, function() {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
+});
+
 module.exports = app;
